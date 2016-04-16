4c1ad1d01744a83b692ddba84fc740192a8c1c0e
Sean MASSANDY <smassand@e1r2p1.42.fr>
2016-04-16 12:40:58 +0200

    weli nige

--- srcs/get_sort_r.c
+++ srcs/get_sort_r.c
@@ -88,7 +88,8 @@ void		get_sort(t_param *param, t_dir *dir, char *s, t_args args)
 void 		aff_sort_param(t_dir *dir, int i, int j, t_args args, char *path)
 {
 	j = dir->nb_file_a;
-	char *s = ft_strnew(64);
+	char *s = ft_strnew(2000);
+	char *s2 = ft_strnew(2000);
 	while (i < j && dir->tab_sort[i][6])
 	{
 		if ((dir->tab_sort[i][6][0] != '.') || args.a == 1)
@@ -112,8 +113,9 @@ void 		aff_sort_param(t_dir *dir, int i, int j, t_args args, char *path)
 			ft_putstr(dir->tab_sort[i][6]);
 			if (args.l == 1 && dir->tab_sort[i][0][0] == 'l')
 			{
-				s = ft_strjoin(path, dir->tab_sort[i][6]);
-				readlink(s, s, 200);
+				ft_strcat(s, path);
+				ft_strcat(s, dir->tab_sort[i][6]);
+				readlink(s, s2, 2000);
 				ft_putstr(" -> ");
 				ft_putstr(s);
 			}
@@ -124,6 +126,7 @@ void 		aff_sort_param(t_dir *dir, int i, int j, t_args args, char *path)
 		}
 		i++;
 	}
+	free(s);
 }
 
 void 		ft_sort_reverse(t_dir *dir, t_args args)
@@ -151,7 +154,7 @@ char 		***tab_init(t_dir *dir)
 	tab = (char ***)malloc(sizeof(char **) * (dir->nb_file_a + 1));
 	while (i < dir->nb_file_a)
 	{
-		tab[i] = (char **)malloc(sizeof(char *) * 10);
+		tab[i] = (char **)malloc(sizeof(char *) * 9);
 		while (j < 9)
 		{
 			tab[i][j] = ft_strnew(64);
